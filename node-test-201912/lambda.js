fc91a61a95598413b784e9713717b7a34c82e53e
wmw1490 <wmw1490@yahoo.com>
2019-11-30 11:20:35 -0500

    first deploy

--- node-test-201912/lambda.js
+++ node-test-201912/lambda.js
@@ -1,23 +1,24 @@
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
+    debugger;
 
     const https = require('https');
 
     https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-      let data = '';
+        let data = '';
 
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
 
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log(JSON.parse(data).explanation);
-      });
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(JSON.parse(data).explanation);
+        });
 
     }).on("error", (err) => {
-      console.log("Error: " + err.message);
+        console.log("Error: " + err.message);
     });
 
-    callback(null, {"message": "Successfully executed"});
+    callback(null, { "message": "Successfully executed" });
 }
